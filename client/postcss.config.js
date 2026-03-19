a7716600d4cb0fc04a08b7d074eee90bb501f54f
Pugazhendhi <pugazhjs9@gmail.com>
2026-03-19 15:57:41 +0530

    Bypassed past 2 commits

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,12 +1,6 @@
-/* eslint-env node */
-
-const isTest = process.env.NODE_ENV === "test";
-
 export default {
-  plugins: isTest
-    ? []
-    : {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
 };
