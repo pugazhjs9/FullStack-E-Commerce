07164ea58a1ff4111643b02322be0cd6623b6d49
Pugazhendhi <pugazhjs9@gmail.com>
2026-05-06 17:07:00 +0530

    Fix ESLint no-unused-vars error for 'command' in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig(({ mode }) => ({
   base: "/",
   plugins: [react()],
   css: mode === "test" ? false : undefined,
